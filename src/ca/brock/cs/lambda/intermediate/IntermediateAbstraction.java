3863290766beb8b170caacf46fe94057a73a87c5
Robert Antwi <robert45antwi@gmail.com>
2026-03-10 12:32:14 -0400

    Updated Partial evaluator, added a test class updated IntermediateMatch, InterMediateAbstraction in the process

--- src/ca/brock/cs/lambda/intermediate/IntermediateAbstraction.java
+++ src/ca/brock/cs/lambda/intermediate/IntermediateAbstraction.java
@@ -6,6 +6,7 @@ import ca.brock.cs.lambda.combinators.KCombinator;
 import ca.brock.cs.lambda.combinators.CombinatorApplication;
 import ca.brock.cs.lambda.CombinatorTranslator; // Import the static helper
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -71,12 +72,91 @@ public class IntermediateAbstraction extends IntermediateTerm {
      * @return An IntermediateTerm with this layer of abstraction eliminated.
      */
     @Override
-    public IntermediateTerm methodT(boolean optimize) {
-//        // This method is called as T[λx.E] where 'this' is the Abstraction 'λx.E'.
-//        // So, 'x' is 'this.parameter' and 'E' is 'this.body'.
-//        // The body must first be processed by methodT() to remove any inner lambdas.
-//        IntermediateTerm transformedBody = body.methodT(optimize); // Pass optimize flag down
-//        return CombinatorTranslator.transformAbstraction(parameter, transformedBody, optimize); // Pass optimize flag down
+//    public IntermediateTerm methodT(boolean optimize) {
+////        // This method is called as T[λx.E] where 'this' is the Abstraction 'λx.E'.
+////        // So, 'x' is 'this.parameter' and 'E' is 'this.body'.
+////        // The body must first be processed by methodT() to remove any inner lambdas.
+////        IntermediateTerm transformedBody = body.methodT(optimize); // Pass optimize flag down
+////        return CombinatorTranslator.transformAbstraction(parameter, transformedBody, optimize); // Pass optimize flag down
+//        // First, recursively apply methodT to the body to ensure inner lambdas are eliminated first.
+//        IntermediateTerm transformedBody = body.methodT(optimize);
+//
+//        // Rule 1: T[λx.x] -> I
+//        if (transformedBody instanceof IntermediateVariable) {
+//            if (((IntermediateVariable) transformedBody).getName().equals(parameter)) {
+//                return new IntermediateICombinator();
+//            }
+//        }
+//
+//        // Rule 2: T[λx.E] -> (K T[E]) if x is not free in E
+//        // This is a crucial base case that handles all constants and other terms where the
+//        // abstracted variable is not used.
+//        if (!transformedBody.getFreeVariables().contains(parameter)) {
+//            return new IntermediateApplication(new IntermediateKCombinator(), transformedBody);
+//        }
+//
+//        // Rule 3-5: T[λx.(E1 E2)] -> ...
+//        if (transformedBody instanceof IntermediateApplication) {
+//            IntermediateApplication application = (IntermediateApplication) transformedBody;
+//            IntermediateTerm func = application.getFunction();
+//            IntermediateTerm arg = application.getArgument();
+//
+//            boolean xFreeInFunc = func.getFreeVariables().contains(parameter);
+//            boolean xFreeInArg = arg.getFreeVariables().contains(parameter);
+//
+//            // Optimization 1 (Rule 4): T[λx.(E1 E2)] -> (C T[λx.E1] T[E2]) if x is free in E1 but not E2
+//            // This rule is only applied if the 'optimize' flag is true.
+//            if (optimize && xFreeInFunc && !xFreeInArg) {
+//                IntermediateTerm transformedFunc = new IntermediateAbstraction(parameter, func).methodT(optimize);
+//                return new IntermediateApplication(
+//                    new IntermediateApplication(new IntermediateCCombinator(), transformedFunc),
+//                    arg
+//                );
+//            }
+//
+//            // Optimization 2 (Rule 5): T[λx.(E1 E2)] -> (B T[E1] T[λx.E2]) if x is free in E2 but not E1
+//            // This rule is also only applied if the 'optimize' flag is true.
+//            if (optimize && !xFreeInFunc && xFreeInArg) {
+//                IntermediateTerm transformedArg = new IntermediateAbstraction(parameter, arg).methodT(optimize);
+//                return new IntermediateApplication(
+//                    new IntermediateApplication(new IntermediateBCombinator(), func),
+//                    transformedArg
+//                );
+//            }
+//
+//            // Default Application Rule (Rule 3): T[λx.(E1 E2)] -> (S T[λx.E1] T[λx.E2])
+//            // This is the most general rule and is used when the above optimizations don't apply,
+//            // or if the 'optimize' flag is false.
+//            IntermediateTerm transformedFunc = new IntermediateAbstraction(parameter, func).methodT(optimize);
+//            IntermediateTerm transformedArg = new IntermediateAbstraction(parameter, arg).methodT(optimize);
+//            return new IntermediateApplication(
+//                new IntermediateApplication(new IntermediateSCombinator(), transformedFunc),
+//                transformedArg
+//            );
+//        }
+//
+//        // This case should theoretically not be reached if all abstractions have been handled
+//        // recursively, but serves as a failsafe.
+//        // If the body is a single term where 'x' is free, but it's not an application,
+//        // we can't reduce it further with the standard rules. This could happen with
+//        // an expression like `λx.(x)`.
+//        return this;
+//
+//    }
+    /**
+     * Applies the T[] transformation rules to eliminate this lambda abstraction.
+     * This method implements the core rules for T[λx.E].
+     * @param optimize A flag indicating whether to apply optimizations (e.g., B and C combinators).
+     * @return An IntermediateTerm with this layer of abstraction eliminated.
+     */
+
+/**
+ * Applies the T[] transformation rules to eliminate this lambda abstraction.
+ * This method implements the core rules for T[λx.E].
+ * @param optimize A flag indicating whether to apply optimizations (e.g., B and C combinators).
+ * @return An IntermediateTerm with this layer of abstraction eliminated.
+ */
+       public IntermediateTerm methodT(boolean optimize) {
         // First, recursively apply methodT to the body to ensure inner lambdas are eliminated first.
         IntermediateTerm transformedBody = body.methodT(optimize);
 
@@ -88,8 +168,6 @@ public class IntermediateAbstraction extends IntermediateTerm {
         }
 
         // Rule 2: T[λx.E] -> (K T[E]) if x is not free in E
-        // This is a crucial base case that handles all constants and other terms where the
-        // abstracted variable is not used.
         if (!transformedBody.getFreeVariables().contains(parameter)) {
             return new IntermediateApplication(new IntermediateKCombinator(), transformedBody);
         }
@@ -104,7 +182,6 @@ public class IntermediateAbstraction extends IntermediateTerm {
             boolean xFreeInArg = arg.getFreeVariables().contains(parameter);
 
             // Optimization 1 (Rule 4): T[λx.(E1 E2)] -> (C T[λx.E1] T[E2]) if x is free in E1 but not E2
-            // This rule is only applied if the 'optimize' flag is true.
             if (optimize && xFreeInFunc && !xFreeInArg) {
                 IntermediateTerm transformedFunc = new IntermediateAbstraction(parameter, func).methodT(optimize);
                 return new IntermediateApplication(
@@ -114,7 +191,6 @@ public class IntermediateAbstraction extends IntermediateTerm {
             }
 
             // Optimization 2 (Rule 5): T[λx.(E1 E2)] -> (B T[E1] T[λx.E2]) if x is free in E2 but not E1
-            // This rule is also only applied if the 'optimize' flag is true.
             if (optimize && !xFreeInFunc && xFreeInArg) {
                 IntermediateTerm transformedArg = new IntermediateAbstraction(parameter, arg).methodT(optimize);
                 return new IntermediateApplication(
@@ -124,8 +200,6 @@ public class IntermediateAbstraction extends IntermediateTerm {
             }
 
             // Default Application Rule (Rule 3): T[λx.(E1 E2)] -> (S T[λx.E1] T[λx.E2])
-            // This is the most general rule and is used when the above optimizations don't apply,
-            // or if the 'optimize' flag is false.
             IntermediateTerm transformedFunc = new IntermediateAbstraction(parameter, func).methodT(optimize);
             IntermediateTerm transformedArg = new IntermediateAbstraction(parameter, arg).methodT(optimize);
             return new IntermediateApplication(
@@ -134,13 +208,26 @@ public class IntermediateAbstraction extends IntermediateTerm {
             );
         }
 
-        // This case should theoretically not be reached if all abstractions have been handled
-        // recursively, but serves as a failsafe.
-        // If the body is a single term where 'x' is free, but it's not an application,
-        // we can't reduce it further with the standard rules. This could happen with
-        // an expression like `λx.(x)`.
-        return this;
+        // --- CORRECTED RULE: Distribute Abstraction to Input and Cases ---
+        if (transformedBody instanceof IntermediateMatch) {
+            IntermediateMatch match = (IntermediateMatch) transformedBody;
 
+            // 1. Abstract over the input expression
+            IntermediateTerm abstractedInput = new IntermediateAbstraction(parameter, match.getInput()).methodT(optimize);
+
+            // 2. Abstract over every branch result
+            List<IntermediateMatch.Case> abstractedCases = new java.util.ArrayList<>();
+            for (IntermediateMatch.Case c : match.getCases()) {
+                // Distribute the abstraction into the compiled branch body
+                IntermediateTerm abstractedBranch = new IntermediateAbstraction(parameter, c.getResult()).methodT(optimize);
+                abstractedCases.add(new IntermediateMatch.Case(c.getPattern(), abstractedBranch));
+            }
+
+            return new IntermediateMatch(abstractedInput, abstractedCases);
+        }
+
+        // Failsafe
+        return this;
     }
 
 
